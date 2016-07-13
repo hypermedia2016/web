4b7e6b849bf366e9e4344df8a41114cae65106dd
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-13 15:59:13 +0200

    auto deploy fixed

--- WEBSITE/public/pull.php
+++ WEBSITE/public/pull.php
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-system('git pull');
+system('git pull origin master');
 system('cd ..');
 system('npm install');
 system('gulp --production');
