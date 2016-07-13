359a1c5ef64173819c60efb34436362e71bf9b3a
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-13 16:22:54 +0200

    composer disabled

--- WEBSITE/resources/php/include/bootstrap.php
+++ WEBSITE/resources/php/include/bootstrap.php
@@ -21,7 +21,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+//require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../../../config/config.php';
 
 $connection = getConnection();
