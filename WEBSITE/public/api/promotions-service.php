4f4aae0bbacd8fff1da24221b588e522a2402c0c
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-14 16:56:55 +0200

    minor fix

--- WEBSITE/public/api/promotions-service.php
+++ WEBSITE/public/api/promotions-service.php
@@ -8,7 +8,8 @@
 
 require_once __DIR__ . '/../../resources/php/include/bootstrap.php';
 
-$query = 'SELECT P.*, S.id as service_id FROM promotions_service P JOIN services S on P.service_id = S.id';
+//$query = 'SELECT P.*, S.id as service_id FROM promotions_service P JOIN services S on P.service_id = S.id';
+$query = 'SELECT P.* FROM promotions_service';
 $promotions = getDataByQuery($query);
 
 print json_encode($promotions);
