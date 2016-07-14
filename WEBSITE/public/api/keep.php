3288a472d83109b1fce6d289a43cc96f6bb325ba
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-14 15:00:44 +0200

    minor fix

--- WEBSITE/public/api/keep.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: claudio
- * Date: 24/05/16
- * Time: 13.25
- */
--- WEBSITE/public/api/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: claudio
- * Date: 24/05/16
- * Time: 13.25
- */
-
-print json_encode(['test1'=>'test11', 'test2'=>'test22']);
