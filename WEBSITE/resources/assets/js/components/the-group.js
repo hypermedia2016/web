92a46cee7ff78c6500694b0e58bf314cc653bff1
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-16 12:03:23 +0200

    news inserted in is and jade

--- WEBSITE/resources/assets/js/components/the-group.js
+++ WEBSITE/resources/assets/js/components/the-group.js
@@ -7,10 +7,14 @@ Vue.component('the-group', {
     props: ['locations'],
     data() {
         return {
+            news: []
         }
 
     },
     ready() {
+        //get news
+        this.loadNews();
+
         //add current location
         this.locations.push({name: 'The group', url: 'the-group.html'});
 
@@ -25,6 +29,19 @@ Vue.component('the-group', {
     },
 
     methods:{
-
+        loadNews(){
+            var _this = this;
+            this.$http({url: basicUrl + '/api/news.php', method: 'GET'}).then( (response) => {
+                if(response.data.error != undefined){
+                    _this.error = response.data.error;
+                }else {
+                    var tmp = response.data;
+                    _this.news = tmp;
+                    _this.error = '';
+                }
+            }, function (response) {
+                _this.error = 'Loading error...';
+            });
+        },
     }
 });
